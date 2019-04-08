bbfced4cfe4b4451eae1815edfac31690cbaee46
Saksham1970 <45041294+Saksham1970@users.noreply.github.com>
2019-04-08 18:57:06 +0530

    Establishing BeginPlay

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -2,6 +2,18 @@
 
 #include "Tank.h"
 #include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	ATank* ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Error, TEXT("Tank not Possessed"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Tank Possessed : %s"),*ControlledTank->GetName());
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
 }
